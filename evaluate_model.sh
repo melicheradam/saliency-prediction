608e83d14045a4dd9f53dd116d65ff671f0d66b0
Adam Melicher <addik36@gmail.com>
2022-03-06 20:09:55 +0100

    make new scripts for models training and evaluation

---
+++ evaluate_model.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+###
+#
+#
+
+MODEL="nothing"
+MODEL_NAME="nothing"
+OBSERVER="nothing"
+
+function parse_args(){
+  while getopts "h?tno:" opt; do
+    case "$opt" in
+      h|\?)
+        echo "Available arguments:"
+        echo "  -t <model_type>       generalized, personalized"
+        echo "  -n <model_name>       name of the model which will be serialized"
+        echo "  -o <OBSERVER>         name of observer"
+        exit 0
+        ;;
+      t)  MODEL=$OPTARG
+        ;;
+      n)  MODEL_NAME=$OPTARG
+        ;;
+      o)  OBSERVER=$OPTARG
+        ;;
+    esac
+  done
+
+  # if user does not set model name set it same as model type
+  if [ "$MODEL_NAME" = "nothing" ]
+  then
+    MODEL_NAME=$MODEL
+  fi
+}
+
+parse_args
+
+if [ "$MODEL" = "generalized" ]
+then
+  GT_PATH='data/PSD/orig_generalized'
+  echo "Loading model named ..."
+  encoder-decoder-model/load_model.sh "$MODEL_NAME"
+elif [ "$MODEL" = "personalized" ]
+then
+  GT_PATH='data/PSD/fix_orig'
+  echo "Loading model named $MODEL_NAME""_""$OBSERVER..."
+  encoder-decoder-model/load_model.sh "$MODEL_NAME""_""$OBSERVER"
+else
+  echo "Incorrect model type, valid options are: generalized, personalized"
+  echo "Exiting..."
+  exit 0
+fi
+
+echo "Predicting maps by $MODEL_NAME model..."
+rm -rf test-results/"$MODEL_NAME"/"$OBSERVER"
+mkdir -p test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-discrepancy
+mkdir -p test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
+
+## Predict saliency maps for the test image
+sudo docker run -u root -v $(pwd):/labs -it --gpus all python3-tensorflow-dp python encoder-decoder-model/main.py test -d personalized -p encoder-decoder-model/data/personalized/test
+cp -rf encoder-decoder-model/results/images test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
+
+## Evaluate all saliency metrics
+echo "Evaluating performance..."
+sudo docker run -u root -v $(pwd):/labs -it python2-dp python src/evaluate_results.py -gt $GT_PATH/"$OBSERVER" -raw $GT_PATH/"$OBSERVER" -sal test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency -output  test-results/"$MODEL_NAME"/"$OBSERVER"/personalized.json -pg test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
+
+## Produce discrepancy maps for the observer's predictions
+echo "Producing discrepancy maps..."
+sudo docker run -u root -v $(pwd):/labs -it python3-tensorflow-dp python src/differentiate_maps.py -gt $GT_PATH/"$OBSERVER" -sal test-results/"$MODEL_NAME"/"$OBSERVER"/saliency -out test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-discrepancy -orig encoder-decoder-model/data/personalized/test
+
--- train_model.sh
+++ train_model.sh
@@ -3,25 +3,106 @@
 #
 #
 
-# PSD Dataset
-GENERATED_GENERALIZED_MAPS_PATH='data/PSD/orig_generalized'
-GENERATED_PERSONALIZED_MAPS_PATH='data/PSD/fixations/Personalized_maps'
-ORIGINAL_IMAGE_STIMULI_PATH='data/PSD/images'
-ORIGINAL_PERSONALIZED_MAPS_PATH='data/PSD/fix_orig'
+MODEL="nothing"
+MODEL_NAME="nothing"
+OBSERVER="nothing"
 
-MODEL=$1
+function parse_args(){
+  while getopts "h?tno:" opt; do
+    case "$opt" in
+      h|\?)
+        echo "Available arguments:"
+        echo "  -t <model_type>       generalized, personalized"
+        echo "  -n <model_name>       name of the model which will be serialized"
+        echo "  -o <observer_name>    name of observer"
+        exit 0
+        ;;
+      t)  MODEL=$OPTARG
+        ;;
+      n)  MODEL_NAME=$OPTARG
+        ;;
+      o)  OBSERVER=$OPTARG
+        ;;
+    esac
+  done
+
+  # if user does not set model name set it same as model type
+  if [ "$MODEL_NAME" = "nothing" ]
+  then
+    MODEL_NAME=$MODEL
+  fi
+}
+
+# PSD Dataset
+IMAGE_PATH='data/PSD/images'
+
+parse_args
 
 if [ "$MODEL" = "generalized" ]
 then
-  SAL =
+  GT_PATH='data/PSD/orig_generalized'
   echo "$MODEL"
 elif [ "$MODEL" = "personalized" ]
 then
+  GT_PATH='data/PSD/fix_orig'
   echo "$MODEL"
 else
-  echo "Incorrect arguments, valid options are: generalized, personalized"
+  echo "Incorrect model type, valid options are: generalized, personalized"
   echo "Exiting..."
   exit 0
 fi
+
+
+function train_generalized(){
+  echo "Loading salicon model..."
+  encoder-decoder-model/load_model.sh salicon
+
+  # Copy all stimuli files and generalized saliency maps
+  echo "Copying stimuli files..."
+  sudo rm -rf encoder-decoder-model/data/personalized/stimuli
+  sudo rm -rf encoder-decoder-model/data/personalized/val-stimuli
+  cp -r $IMAGE_PATH encoder-decoder-model/data/personalized/stimuli
+
+  echo "Copying generalized saliency maps..."
+  sudo rm -rf encoder-decoder-model/data/personalized/saliency
+  sudo rm -rf encoder-decoder-model/data/personalized/val-saliency
+  cp -r $GT_PATH encoder-decoder-model/data/personalized/saliency
+
+  # Train the generalized model
+  echo "Training the generalized model on generalized saliency maps from the personalized dataset..."
+  sudo docker run -u root -v $(pwd):/labs -it --gpus all python3-tensorflow-dp python encoder-decoder-model/main.py train -d personalized
+
+  ## Serialize the generalized model in case it is needed in future
+  echo "Serializing the generalized model..."
+  sudo bash encoder-decoder-model/serialize_current_model.sh "$MODEL_NAME"
+}
+
+function train_personalized(){
+  # Copy all stimuli files
+  echo "Copying stimuli files..."
+  sudo rm -rf encoder-decoder-model/data/personalized/stimuli
+  sudo rm -rf encoder-decoder-model/data/personalized/val-stimuli
+  cp -r $IMAGE_PATH encoder-decoder-model/data/personalized/stimuli
+
+  # Train a model personalized for a chosen observer
+  echo "Copying saliency maps..."
+  sudo rm -rf encoder-decoder-model/data/personalized/saliency
+  sudo rm -rf encoder-decoder-model/data/personalized/val-saliency
+  cp -r $GT_PATH/$OBSERVER encoder-decoder-model/data/personalized/saliency
+
+  echo "Training personalized model..."
+  sudo docker run -u root -v $(pwd):/labs -it --gpus all python3-tensorflow-dp python encoder-decoder-model/main.py train -d personalized
+
+  ## Serialize the generalized model in case it is needed in future
+  echo "Serializing the personalized model..."
+  sudo bash encoder-decoder-model/serialize_current_model.sh "$MODEL_NAME""_""$OBSERVER"
+}
+
+
+
+
+
+
+
 
 
