2104e3598d8dc3e1db2d19ffaa8673eab7ce94e5
Adam Melicher <addik36@gmail.com>
2022-03-21 10:15:54 +0100

    evaluating cat2000

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -5,7 +5,7 @@ declare -a OBSERVER_NAMES=($(ls data/PSD/fix_orig))
 i=1
 for observer in "${OBSERVER_NAMES[@]}";do
   echo "------- $observer --------"
-  sudo bash train_model.sh -t personalized -n run1 -o $observer
-
+  # sudo bash train_model.sh -t personalized -n run1 -o $observer
+  sudo bash evaluate_model.sh -t cat2000 -n run1 -o $observer
   i=$(expr $i + 1)
 done
--- evaluate_model.sh
+++ evaluate_model.sh
@@ -41,6 +41,11 @@ then
   GT_PATH='data/PSD/fix_orig'
   echo "Loading model named $MODEL_NAME""_""$OBSERVER..."
   encoder-decoder-model/load_model.sh "$MODEL_NAME""_""$OBSERVER"
+elif [ "$MODEL" = "cat2000" ]
+then
+  GT_PATH='data/cat2000/fixations'
+  echo "Loading model named $MODEL_NAME""_""$OBSERVER..."
+  encoder-decoder-model/load_model.sh "$MODEL_NAME""_""$OBSERVER"
 else
   echo "Incorrect model type, valid options are: generalized, personalized"
   echo "Exiting..."
@@ -48,19 +53,24 @@ else
 fi
 
 echo "Predicting maps by $MODEL_NAME model..."
-rm -rf test-results/"$MODEL_NAME"/"$OBSERVER"
+rm -rf test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-discrepancy
+rm -rf test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
+
 mkdir -p test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-discrepancy
 mkdir -p test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
 
 ## Predict saliency maps for the test image
-sudo docker run -u root -v $(pwd):/labs -it --gpus all python3-tensorflow-dp python encoder-decoder-model/main.py test -d personalized -p encoder-decoder-model/data/personalized/test
+# sudo docker run -u root -v $(pwd):/labs -it --gpus all python3-tensorflow-dp python encoder-decoder-model/main.py test -d personalized -p encoder-decoder-model/data/personalized/test
+sudo docker run -u root -v $(pwd):/labs -it --gpus all python3-tensorflow-dp python encoder-decoder-model/main.py test -d personalized -p data/cat2000/images
+
 cp -rf encoder-decoder-model/results/images/. test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
 
 ## Evaluate all saliency metrics
-echo "Evaluating performance..."
-sudo docker run -u root -v $(pwd):/labs -it python2-dp python src/evaluate_results.py -gt $GT_PATH/"$OBSERVER" -raw $GT_PATH/"$OBSERVER" -sal test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency -output  test-results/"$MODEL_NAME"/"$OBSERVER"/personalized.json -pg test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
+# echo "Evaluating performance..."
+# sudo docker run -u root -v $(pwd):/labs -it python2-dp python src/evaluate_results.py -gt $GT_PATH/"$OBSERVER" -raw $GT_PATH/"$OBSERVER" -sal test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency -output  test-results/"$MODEL_NAME"/"$OBSERVER"/personalized.json -pg test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency
 
 ## Produce discrepancy maps for the observer's predictions
 echo "Producing discrepancy maps..."
-sudo docker run -u root -v $(pwd):/labs -it python3-tensorflow-dp python src/differentiate_maps.py -gt $GT_PATH/"$OBSERVER" -sal test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency -out test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-discrepancy -orig encoder-decoder-model/data/personalized/test
+# sudo docker run -u root -v $(pwd):/labs -it python3-tensorflow-dp python src/differentiate_maps.py -gt $GT_PATH/"$OBSERVER" -sal test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency -out test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-discrepancy -orig encoder-decoder-model/data/personalized/test
+sudo docker run -u root -v $(pwd):/labs -it python3-tensorflow-dp python src/differentiate_maps.py -gt $GT_PATH -sal test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-saliency -out test-results/"$MODEL_NAME"/"$OBSERVER"/"$MODEL"-discrepancy -orig data/cat2000/images
 
